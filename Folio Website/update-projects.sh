6ee3ef2929e97d8aaed54a289ff5e1f96845ca12
Connor <connorturlan@gmail.com>
2022-07-08 15:04:17 +0930

    update portfolio

---
+++ Folio Website/update-projects.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp '../JavaScript Calculator/pages/index.html' './pages/calc/calc.html'
+cp '../JavaScript Calculator/scripts/calculator.js' './pages/calc/calculator.js'
+cp '../JavaScript Calculator/styles/calc/style.css' './pages/calc/style.css'
