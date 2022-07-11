a20898430029e0adff140f7350eb44ec41fbd03a
Connor <connorturlan@gmail.com>
2022-07-11 09:47:54 +0930

    improved contact styles

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,5 @@
-function copyToClipboard() {
+// copy the content of the email contact element to the clipboard.
+function copyEmailToClipboard() {
 	let text = document.getElementById("liEmail").getAttribute("value");
 	navigator.clipboard.writeText(text);
 	alert("Email copied to clipboard!");
